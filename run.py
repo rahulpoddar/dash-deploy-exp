9ddacefd05a9cb861b913e0d892b6fa2b417ebf7
Poddar <PODDARA1@novartis.net>
2020-04-13 18:21:49 +0530

    Replaced with csv

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import dash_table
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 from flask import Flask
 import os
 
