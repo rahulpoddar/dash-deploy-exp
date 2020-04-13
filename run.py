d561ee93b226d28adee618fae18de768e53d32f3
Poddar <PODDARA1@novartis.net>
2020-04-13 16:46:08 +0530

    Updated run.py

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+import pandas as pd
+import dash_table
+from dash.dependencies import Input, Output, State
 from flask import Flask
 import os
 
