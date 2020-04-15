d2eeaae21e03ed178ae76a3a5c070e84ff1a62f9
rahulpoddar <rahul.pod@gmail.com>
2020-04-15 17:22:09 +0530

    Update run.py

--- run.py
+++ run.py
@@ -13,7 +13,33 @@ server.secret_key = os.environ.get('secret_key', 'secret')
 app = dash.Dash(name = __name__, server = server)
 app.config.supress_callback_exceptions = True
 
-#df = pd.read_excel('TASK1_annotated_1.xlsx', sheet_name = 'Newone')
+df = pd.read_csv('https://raw.githubusercontent.com/rahulpoddar/dash-deploy-exp/master/TASK1_annotated_1.csv', encoding='latin1')
+
+tasks = df['Kaggle Task name'].unique().tolist()
+
+def generate_summary(task):
+    return 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+def generate_table(dff):
+    rows = []
+    for i in range(len(dff)):
+        row = []
+        for col in ['Document id_', 'Output']:
+            value = dff.iloc[i][col]
+            if col == 'Document id_':
+                cell = html.Td(html.A(href='https://www.google.com/', children = value))
+            else:
+                cell = html.Td(children = value)
+            row.append(cell)
+        rows.append(html.Tr(row))
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in ['Document ID', 'Search Output']]) ] +
+        # Body
+        rows
+    )
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
